ee4dee23a7add3110d79afb88ac97f55c6b3930e
LiuCong <cong.sunshine.library@gmail.com>
2013-01-12 02:06:37 +0800

    complete the UI

---
+++ assets/javascripts/templates/index.js
@@ -0,0 +1,7 @@
+jQuery(function($) {
+    Templates = {
+        subject: _.template($("#subject_template").html()),
+        lesson: _.template($("#lesson_template").html()),
+		month : _.template($("#month_template").html())
+    };
+});
--- src/org/sunshine_library/exercise/app/activity/MainActivity.java
+++ src/org/sunshine_library/exercise/app/activity/MainActivity.java
@@ -2,9 +2,16 @@ package org.sunshine_library.exercise.app.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.webkit.JsResult;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
 import org.sunshine_library.exercise.R;
 
+
 public class MainActivity extends Activity {
+	private WebView content;
     /**
      * Called when the activity is first created.
      */
@@ -12,5 +19,21 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        content = (WebView) findViewById(R.id.content);
+        WebSettings settings = content.getSettings();
+        settings.setJavaScriptEnabled(true);
+        content.setWebChromeClient(new WebChromeClient() {
+
+			@Override
+			public boolean onJsAlert(WebView view, String url, String message,
+					JsResult result) {
+				// TODO Auto-generated method stub
+				return super.onJsAlert(view, url, message, result);
+			}
+        	
+        });
+        
+        content.loadUrl("file:///android_asset/index.html");
     }
 }
