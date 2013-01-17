2b97e16ccf78608318dad6b131b35b957e0a39d3
chenhao <chenhao9255@gmail.com>
2013-01-17 15:12:36 +0800

    service & thread

--- src/org/sunshine_library/exercise/app/activity/MainActivity.java
+++ src/org/sunshine_library/exercise/app/activity/MainActivity.java
@@ -6,24 +6,25 @@ import android.app.Activity;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.view.View.OnClickListener;
+import android.util.Log;
 import android.webkit.JsResult;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
-import android.widget.Button;
 
 
 public class MainActivity extends Activity {
+	private  final static String TAG_MAINACTIVITY = "MainActivity";
 	private WebView content;
 	
-	private Button button_1;
 	private NotificationManager notificationManager;
+	private Notification notificationBar;
 	private PendingIntent pendingIntent;
+	
+	private NotificationBarService mNotificationBarService;
+	public static NotificationMsgHandler notificationMsgHandler;
 	
     /**
      * Called when the activity is first created.
@@ -32,6 +33,13 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        notificationMsgHandler = new NotificationMsgHandler(this);
+        
+        mNotificationBarService = new NotificationBarService(notificationMsgHandler);
+        
+        new Thread(mNotificationBarService).run();
+        
         
         content = (WebView) findViewById(R.id.content);
         WebSettings settings = content.getSettings();
@@ -49,96 +57,63 @@ public class MainActivity extends Activity {
         
         content.loadUrl("file:///android_asset/index.html");
         
-        init();
-        //initNotify();
+       
     }
     
-   private void init()
-   {
-	   button_1 = (Button)findViewById(R.id.button_1);
-	   button_1.setOnClickListener(onClickListener);
-	   notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE); 
-	   Intent intent = new Intent();
-	   pendingIntent = PendingIntent.getActivity(MainActivity.this, 0, intent, 0);
-   }
-   
-   OnClickListener onClickListener = new OnClickListener() {
-	private int Notification_ID_BASE = 110;
-	private Notification baseNF;
-	
-	@Override
-	public void onClick(View v) {
-		// TODO Auto-generated method stub
-		baseNF = new Notification();
-		
-		baseNF.icon = R.drawable.icon;
-		baseNF.tickerText = "clicked";
-		
-		baseNF.defaults |= Notification.DEFAULT_SOUND;  
-		baseNF.defaults |= Notification.DEFAULT_VIBRATE;  
-		baseNF.defaults |= Notification.DEFAULT_LIGHTS;
-		
-		baseNF.flags |= Notification.FLAG_INSISTENT; 
-		baseNF.flags |= Notification.FLAG_AUTO_CANCEL; 
-		
-		baseNF.setLatestEventInfo(MainActivity.this, "hello", "world", pendingIntent);
-		notificationManager.notify(Notification_ID_BASE, baseNF);
-		
-	}
-};
+    public void initNotificationBar(){
+    	
+    	 notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE); 
+		 notificationBar = new Notification();
+		 Intent intent = new Intent();
+		 pendingIntent = PendingIntent.getActivity(MainActivity.this, 0, intent, 0);
+		 
+		// notificationBar.icon = R.drawable.icon;
+		 notificationBar.tickerText = "clicked";
+		 notificationBar.defaults |= Notification.DEFAULT_SOUND;  
+		 notificationBar.defaults |= Notification.DEFAULT_VIBRATE;  
+		 notificationBar.defaults |= Notification.DEFAULT_LIGHTS;	
+		 notificationBar.flags |= Notification.FLAG_INSISTENT; 
+		 notificationBar.flags |= Notification.FLAG_AUTO_CANCEL; 
+		 
+		 notificationBar.setLatestEventInfo(MainActivity.this, "hello", "world", pendingIntent);
+		 notificationManager.notify(0,notificationBar);
+    }
+ }
+//   private void init()
+//   {
+//	   button_1 = (Button)findViewById(R.id.button_1);
+//	   button_1.setOnClickListener(onClickListener);
+//	   notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE); 
+//	   Intent intent = new Intent();
+//	   pendingIntent = PendingIntent.getActivity(MainActivity.this, 0, intent, 0);
+//   }
+//   
+//   OnClickListener onClickListener = new OnClickListener() {
+//	private int Notification_ID_BASE = 110;
+//	private Notification baseNF;
+//	
+//	@Override
+//	public void onClick(View v) {
+//		// TODO Auto-generated method stub
+//		baseNF = new Notification();
+//		
+//		baseNF.icon = R.drawable.icon;
+//		baseNF.tickerText = "clicked";
+//		
+//		baseNF.defaults |= Notification.DEFAULT_SOUND;  
+//		baseNF.defaults |= Notification.DEFAULT_VIBRATE;  
+//		baseNF.defaults |= Notification.DEFAULT_LIGHTS;
+//		
+//		baseNF.flags |= Notification.FLAG_INSISTENT; 
+//		baseNF.flags |= Notification.FLAG_AUTO_CANCEL; 
+//		
+//		baseNF.setLatestEventInfo(MainActivity.this, "hello", "world", pendingIntent);
+//		notificationManager.notify(Notification_ID_BASE, baseNF);
+//		
+//	}
     
-
-	public void initNotify(){
-		String ns = Context.NOTIFICATION_SERVICE;
-
-        NotificationManager mNotificationManager = (NotificationManager) getSystemService(ns);
-
-        //定义通知栏展现的内容信息
-
-        int icon = R.drawable.icon;
-
-        CharSequence tickerText = "我的通知栏标题";
-
-        long when = System.currentTimeMillis();
-
-        Notification notification = new Notification();
-
-      
-
-        //定义下拉通知栏时要展现的内容信息
-
-        Context context = getApplicationContext();
-
-        CharSequence contentTitle = "我的通知栏标展开标题";
-
-        CharSequence contentText = "我的通知栏展开详细内容";
-
-       
-       // Notification noti = new Notification.Builder(context)
-       // .setContentTitle("我的通知栏标展开标题")
-       // .setContentText("我的通知栏展开详细内容")
-       // .setSmallIcon(R.drawable.icon)
-       // .build();
-        
-        Intent notificationIntent = new Intent(this, MainActivity.class);
-
-       PendingIntent contentIntent = PendingIntent.getActivity(this, 0,notificationIntent, 0);
-
-        notification.setLatestEventInfo(context, contentTitle, contentText,contentIntent); 
-
-      //  notification.when = 8000;
-        //用mNotificationManager的notify方法通知用户生成标题栏消息通知
-
-      
-        mNotificationManager.notify(1, notification);
-	
-	}
-
-}
-
-
-
-
+   
+    
 
 
 
