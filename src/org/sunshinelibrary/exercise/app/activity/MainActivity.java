2bedec265e439f8feb3831530b062c819d79bba4
linuo <3er.com.cn@gmail.com>
2013-03-29 18:30:06 +0800

    [linuo] add html5 video tag logic

--- src/org/sunshinelibrary/exercise/app/activity/MainActivity.java
+++ src/org/sunshinelibrary/exercise/app/activity/MainActivity.java
@@ -21,6 +21,7 @@ import org.sunshinelibrary.exercise.app.interfaces.AndroidInterface;
 import org.sunshinelibrary.exercise.app.interfaces.AndroidUIInterface;
 import org.sunshinelibrary.exercise.app.interfaces.WebInteraction;
 import org.sunshinelibrary.exercise.app.service.NotificationService;
+import org.sunshinelibrary.exercise.app.ui.HTML5WebView;
 import org.sunshinelibrary.exercise.metadata.TestCase;
 import org.sunshinelibrary.support.api.UserInfo;
 
@@ -41,9 +42,7 @@ public class MainActivity extends TopActivity implements AndroidUIInterface {
     private static final String ASSETS = "file:///android_asset/";
     private static final String HTML = ".html";
 
-    private WebView mWebView;
-    private VideoView mVideoView;
-    private RelativeLayout mPlayView;
+    private HTML5WebView mWebView;
     private LinearLayout mLinearLayout;
     private AndroidInterface mInterface;
     private WebInteraction mInteraction;
@@ -58,24 +57,27 @@ public class MainActivity extends TopActivity implements AndroidUIInterface {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        mVideoView = (VideoView) findViewById(R.id.vv);
-        mLinearLayout = (LinearLayout) findViewById(R.id.player_loading);
-        mPlayView = (RelativeLayout) findViewById(R.id.play);
-
-        mWebView = (WebView) findViewById(R.id.lesson);
-//        mWebViewCollection.put("stages", (WebView) findViewById(R.id.stage));
-//        mWebViewCollection.put("section", (WebView)findViewById(R.id.lesson));
-//        mWebViewCollection.put("exercise", (WebView) findViewById(R.id.exercise));
-//        mWebViewCollection.put("quiz", (WebView) findViewById(R.id.quiz));
-//        mWebViewCollection.put("summery", (WebView) findViewById(R.id.summery));
+        mLinearLayout = (LinearLayout) findViewById(R.id.web);
+        mWebView = new HTML5WebView(this);
 
         mInterface = ExerciseApplication.getInstance().getSyncManager();
         mInteraction = new WebInteraction(mWebView, this);
         mInterface.setUIInterface(this);
         mInterface.register(mInteraction);
 
-        initWebView();
-        activateWebView();
+//        if (savedInstanceState != null) {
+//            mWebView.restoreState(savedInstanceState);
+//        } else {
+//            mWebView.addJavascriptInterface(mInterface, "android");
+//            mWebView.loadUrl(ASSETS + "index" + HTML);
+//        }
+
+
+        mLinearLayout.addView(mWebView.getLayout(), HTML5WebView.COVER_SCREEN_PARAMS);
+        mWebView.setInteraction(mInteraction);
+        mWebView.addJavascriptInterface(mInterface, "android");
+        mWebView.loadUrl(ASSETS + "index" + HTML);
+
         startService(new Intent(MainActivity.this, NotificationService.class));
 
         Intent intent = new Intent(UserInfo.ACTION_SIGN_IN_ACTIVITY);
@@ -115,109 +117,28 @@ public class MainActivity extends TopActivity implements AndroidUIInterface {
 
     }
 
-    public void openVideoActivity(View v) {
-        mWebView.loadUrl("http://m.youku.com/smartphone/channels?cid=92");
-    }
-
-    public void activateWebView() {
-        mWebView.setVisibility(View.VISIBLE);
-    }
-
-    private void initWebView() {
-        WebChromeClient wcc = new WebChromeClient() {
-            @Override
-            public boolean onJsAlert(WebView view, String url, String message,
-                                     JsResult result) {
-                return super.onJsAlert(view, url, message, result);
-            }
-
-            public boolean onConsoleMessage(ConsoleMessage cm) {
-                Log.d(getClass().getName(), cm.message() + " -- From line "
-                        + cm.lineNumber() + " of "
-                        + cm.sourceId());
-                return true;
-            }
-        };
-
-        WebViewClient wvc = new WebViewClient() {
-            public boolean shouldOverrideUrlLoading(final WebView view,
-                                                    final String url) {
-                if (url.contains("3gp") || url.contains("mp4")) {
-                    playView(Uri.parse(url));
-                } else {
-                    view.loadUrl(url);
-                }
-
-                return true;
-            }// 重写点击动作,用webview载入
-        };
-
-        WebSettings settings = mWebView.getSettings();
-        settings.setJavaScriptEnabled(true);
-        settings.setDomStorageEnabled(true);
-        mWebView.setWebChromeClient(wcc);
-
-        // 过滤调用第三方浏览器。并且解析视频网站播放地址，传给播放器
-        mWebView.setWebViewClient(wvc);
-        mWebView.addJavascriptInterface(mInterface, "android");
-        mWebView.loadUrl(ASSETS + "index" + HTML);
-    }
-
-    // 播放器的效果
-    public void playView(Uri uri) {
-
-        mPlayView.setVisibility(View.VISIBLE);
-        mWebView.setVisibility(View.GONE);
-
-        // 初始化视频显示控件
-        if (uri != null) {
-
-            if (mVideoView != null) {
-                // 加载播放地址
-                mVideoView.setVideoURI(uri);
-            }
-        }
-        if (mVideoView != null)
-            mVideoView.setOnPreparedListener(new OnPreparedListener() {
-
-                @Override
-                public void onPrepared(MediaPlayer mp) {
-                    if (mVideoView != null) {
-                        // 开始播放
-                        mVideoView.start();
-                        mLinearLayout.setVisibility(View.GONE);
-                    }
-
-                }
-            });
-        if (mVideoView != null) {
-            // 显示控制栏
-            mVideoView.setMediaController(new MediaController(this));
-        }
-
-        // 监听播放完的事件。
-        mVideoView.setOnCompletionListener(new OnCompletionListener() {
-
-            @Override
-            public void onCompletion(MediaPlayer mp) {
-                mVideoView.stopPlayback();
-                mPlayView.setVisibility(View.GONE);
-                mWebView.setVisibility(View.VISIBLE);
-            }
-        });
-    }
 
     // 捕捉返回键，首先隐藏播放器，然后判断是否能够返回。
     public void onBackPressed() {
-        Log.i(TAG, "in" + mPlayView.getVisibility());
-        if (mPlayView.getVisibility() == View.VISIBLE) {
-            mVideoView.stopPlayback();
-            mPlayView.setVisibility(View.GONE);
-            mWebView.setVisibility(View.VISIBLE);
+        Log.i(TAG, "in" + mWebView.inCustomView());
+        if (mWebView.inCustomView()) {
+            mWebView.hideCustomView();
             return;
+        } else {
+            mInteraction.backPressed();
         }
+    }
 
-        mInteraction.backPressed();
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mWebView.stopLoading();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+//        mWebView.saveState(outState);
     }
 
     @Override
@@ -285,5 +206,9 @@ public class MainActivity extends TopActivity implements AndroidUIInterface {
         mLoadReady = true;
         if (mSignIn)
             mInterface.autoSync();
+    }
+
+    public void testVideo(View view) {
+        mWebView.loadUrl("http://freebsd.csie.nctu.edu.tw/~freedom/html5/");
     }
 }
---
+++ src/org/sunshinelibrary/exercise/app/ui/HTML5WebView.java
@@ -0,0 +1,218 @@
+package org.sunshinelibrary.exercise.app.ui;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.*;
+import android.webkit.*;
+import android.widget.FrameLayout;
+import org.sunshinelibrary.exercise.R;
+import org.sunshinelibrary.exercise.app.interfaces.WebInteraction;
+
+public class HTML5WebView extends WebView {
+
+    private Context mContext;
+    private MyWebChromeClient mWebChromeClient;
+    private View mCustomView;
+    private FrameLayout mCustomViewContainer;
+    private WebChromeClient.CustomViewCallback mCustomViewCallback;
+
+    private FrameLayout mContentView;
+    private FrameLayout mBrowserFrameLayout;
+    private FrameLayout mLayout;
+
+    private WebInteraction mInteraction;
+
+    static final String LOGTAG = "HTML5WebView";
+
+    private void init(Context context) {
+        mContext = context;
+        Activity a = (Activity) mContext;
+
+        mLayout = new FrameLayout(context);
+
+        mBrowserFrameLayout = (FrameLayout) LayoutInflater.from(a).inflate(R.layout.custom_screen, null);
+        mContentView = (FrameLayout) mBrowserFrameLayout.findViewById(R.id.main_content);
+        mCustomViewContainer = (FrameLayout) mBrowserFrameLayout.findViewById(R.id.fullscreen_custom_content);
+
+        mLayout.addView(mBrowserFrameLayout, COVER_SCREEN_PARAMS);
+
+        mWebChromeClient = new MyWebChromeClient();
+        setWebChromeClient(mWebChromeClient);
+
+        setWebViewClient(new MyWebViewClient());
+
+        // Configure the webview
+        WebSettings s = getSettings();
+        s.setBuiltInZoomControls(true);
+        s.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.NARROW_COLUMNS);
+        s.setUseWideViewPort(true);
+        s.setLoadWithOverviewMode(true);
+        s.setSavePassword(true);
+        s.setSaveFormData(true);
+        s.setJavaScriptEnabled(true);
+
+        // enable navigator.geolocation
+//        s.setGeolocationEnabled(true);
+//        s.setGeolocationDatabasePath("/data/data/org.itri.html5webview/databases/");
+
+        // enable Web Storage: localStorage, sessionStorage
+        s.setDomStorageEnabled(true);
+
+        mContentView.addView(this);
+    }
+
+    public HTML5WebView(Context context) {
+        super(context);
+        init(context);
+    }
+
+    public HTML5WebView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
+
+    public HTML5WebView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init(context);
+    }
+
+    public FrameLayout getLayout() {
+        return mLayout;
+    }
+
+    public boolean inCustomView() {
+        return (mCustomView != null);
+    }
+
+    public void hideCustomView() {
+        mWebChromeClient.onHideCustomView();
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            // activity will handle with onBackPress()
+//            if ((mCustomView == null) && canGoBack()) {
+//                goBack();
+//                return true;
+//            }
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    private class MyWebChromeClient extends WebChromeClient {
+        private Bitmap mDefaultVideoPoster;
+        private View mVideoProgressView;
+
+        @Override
+        public void onShowCustomView(View view, CustomViewCallback callback) {
+            //Log.i(LOGTAG, "here in on ShowCustomView");
+            HTML5WebView.this.setVisibility(View.GONE);
+
+            // if a view already exists then immediately terminate the new one
+            if (mCustomView != null) {
+                callback.onCustomViewHidden();
+                return;
+            }
+
+            mCustomViewContainer.addView(view);
+            mCustomView = view;
+            mCustomViewCallback = callback;
+            mCustomViewContainer.setVisibility(View.VISIBLE);
+
+            // todo: HTML5WebView.this.mInteraction
+            Log.d(LOGTAG, "onShowCustomView");
+        }
+
+        @Override
+        public void onHideCustomView() {
+
+            if (mCustomView == null)
+                return;
+
+            // Hide the custom view.
+            mCustomView.setVisibility(View.GONE);
+
+            // Remove the custom view from its container.
+            mCustomViewContainer.removeView(mCustomView);
+            mCustomView = null;
+            mCustomViewContainer.setVisibility(View.GONE);
+            mCustomViewCallback.onCustomViewHidden();
+
+            HTML5WebView.this.setVisibility(View.VISIBLE);
+
+            // todo: HTML5WebView.this.mInteraction
+            Log.d(LOGTAG, "onHideCustomView");
+
+
+            //Log.i(LOGTAG, "set it to webVew");
+        }
+
+        @Override
+        public Bitmap getDefaultVideoPoster() {
+            //Log.i(LOGTAG, "here in on getDefaultVideoPoster");
+            if (mDefaultVideoPoster == null) {
+                mDefaultVideoPoster = BitmapFactory.decodeResource(
+                        getResources(), R.drawable.default_video_poster);
+            }
+            return mDefaultVideoPoster;
+        }
+
+        @Override
+        public View getVideoLoadingProgressView() {
+            //Log.i(LOGTAG, "here in on getVideoLoadingPregressView");
+
+            if (mVideoProgressView == null) {
+                LayoutInflater inflater = LayoutInflater.from(mContext);
+                mVideoProgressView = inflater.inflate(R.layout.video_loading_progress, null);
+            }
+            return mVideoProgressView;
+        }
+
+        @Override
+        public void onReceivedTitle(WebView view, String title) {
+            ((Activity) mContext).setTitle(title);
+        }
+
+        @Override
+        public void onProgressChanged(WebView view, int newProgress) {
+            ((Activity) mContext).getWindow().setFeatureInt(Window.FEATURE_PROGRESS, newProgress * 100);
+            Log.d(LOGTAG, "onProgressChanged" + view + newProgress);
+        }
+
+//        @Override
+//        public void onGeolocationPermissionsShowPrompt(String origin, GeolocationPermissions.Callback callback) {
+//            callback.invoke(origin, true, false);
+//        }
+
+        public boolean onConsoleMessage(ConsoleMessage cm) {
+            Log.d(getClass().getName(), cm.message() + " -- From line "
+                    + cm.lineNumber() + " of "
+                    + cm.sourceId());
+            return true;
+        }
+
+    }
+
+    private class MyWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            Log.i(LOGTAG, "shouldOverrideUrlLoading: " + url);
+            // don't override URL so that stuff within iframe can work properly
+            // view.loadUrl(url);
+            return false;
+        }
+    }
+
+    public static final FrameLayout.LayoutParams COVER_SCREEN_PARAMS =
+            new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+
+
+    public void setInteraction(WebInteraction interaction) {
+        mInteraction = interaction;
+    }
+}
